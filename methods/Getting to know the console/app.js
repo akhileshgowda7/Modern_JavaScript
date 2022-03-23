8be12c0515a877f49e962efa97483415438e5d7a
akhilesh <akhileshgowda7@gmail.com>
2022-03-23 15:48:09 -0400

    app.js file of usingg and playingg around with console

---
+++ methods/Getting to know the console/app.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+console.log("Greeting");
+console.warn("This is a warning");
+console.time("Hello");
+console.log("Hello World");
+console.log("Greeting");console.log("Hello World");
+console.log("Greeting");console.log("Hello World");
+console.log("Greeting");console.log("Hello World");
+console.log("Greeting");console.log("Hello World");
+console.log("Greeting");console.log("Hello World");
+console.log("Greeting");console.log("Hello World");
+console.log("Greeting");console.log("Hello World");
+console.log("Greeting");console.log("Hello World");
+console.log("Greeting");console.log("Hello World");
+console.log("Greeting");
+console.timeEnd("Hello");
+console.error("This is a error");
+console.table({a:1,b:2})
