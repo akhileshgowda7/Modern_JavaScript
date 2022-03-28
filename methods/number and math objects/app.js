8b4ceacf41c722221f5e3439034ed56dfe18b5b6
akhilesh <akhileshgowda7@gmail.com>
2022-03-28 15:30:50 -0400

    number and math object js code

---
+++ methods/number and math objects/app.js
@@ -0,0 +1,26 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+//simple math
+val = num1 + num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 - num2;
+
+console.log(val);
+
+//Math object
+val = Math.PI;
+console.log(val);
+val = Math.E
+console.log(val);
+val=Math.round(2.4);
+console.log(val);
+val=Math.ceil(2.4);
+console.log(val);
+val=Math.floor(2.8)
+console.log(val);
+val=Math.abs(-2.8);
+console.log(val);
+
