113dfd4b9f41497deca979dae5cfa3b3a3724ea4
akhilesh <akhileshgowda7@gmail.com>
2022-04-11 17:16:52 -0400

    Window object app.js file

---
+++ DOM/Window object/app.js
@@ -0,0 +1,51 @@
+//window methods /objects / properties
+
+
+//Alert
+// alert("Hello World!!");
+
+//Prompt
+// const input = prompt();
+
+// alert(input);
+
+//confirm
+// if(confirm('Are you sure')){
+    // console.log('YES');
+// }else{
+    // console.log('NO');
+// }
+
+//properties
+let val;
+
+//outer height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+//inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+//scroll points
+
+// val = window.scrollX;
+// val = window.scrollX;
+
+//Location
+
+// val = window.location;
+// val = window.location.hostname;
+// val = window.location.port;
+// val = window.location.search;
+
+//redirect
+// window.location.href = 'http://google.com';
+
+//history object
+window.history.go();
+
+//Navigator object
+val = window.navigator.appVersion;
+
+console.log(val );
